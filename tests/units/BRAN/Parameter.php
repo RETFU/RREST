1e60cc0935f7a09cf8f69104ae6a4b1ee7a34dd0
Fabien Furet <fabien.furet@gmail.com>
2016-02-23 22:11:44 +0100

    test: add test for RREST\Parameters

--- tests/units/BRAN/Parameter.php
+++ tests/units/BRAN/Parameter.php
@@ -24,4 +24,175 @@ class Parameter extends atoum
             ->isEqualTo('string')
         ;
     }
+
+    public function testSetType()
+    {
+        $this
+            ->exception(
+                function() {
+                    $this->newTestedInstance('name','float',true);
+                }
+            )
+            ->isInstanceOf('\RuntimeException')
+            ->message->contains('float')
+        ;
+    }
+
+    public function testAssertValue()
+    {
+        //Required
+        $this->newTestedInstance('name','string',true);
+        $this
+            ->boolean($this->testedInstance->getRequired())
+            ->isTrue();
+        $this
+            ->exception(
+                function() {
+                    $this
+                        ->testedInstance
+                        ->assertValue(null, null)
+                    ;
+                }
+            )
+            ->isInstanceOf('RREST\Exception\InvalidParameterException')
+        ;
+
+        //cast type string
+        $this
+            ->exception(
+                function() {
+                    $this
+                        ->testedInstance
+                        ->assertValue(50, '50')
+                    ;
+                }
+            )
+            ->isInstanceOf('RREST\Exception\InvalidParameterException')
+        ;
+        $this
+            ->exception(
+                function() {
+                    $this
+                        ->testedInstance
+                        ->assertValue(true, 'true')
+                    ;
+                }
+            )
+            ->isInstanceOf('RREST\Exception\InvalidParameterException')
+        ;
+
+        //cast type integer
+        $this->newTestedInstance('name','integer',true);
+        $this
+            ->exception(
+                function() {
+                    $this
+                        ->testedInstance
+                        ->assertValue(50.5, '50.5')
+                    ;
+                }
+            )
+            ->isInstanceOf('RREST\Exception\InvalidParameterException')
+        ;
+
+        //cast type number
+        $this->newTestedInstance('name','number',true);
+        $this
+            ->exception(
+                function() {
+                    $this
+                        ->testedInstance
+                        ->assertValue('test', 'test')
+                    ;
+                }
+            )
+            ->isInstanceOf('RREST\Exception\InvalidParameterException')
+        ;
+
+        //cast type boolean
+        $this->newTestedInstance('name','boolean',true);
+        $this
+            ->exception(
+                function() {
+                    $this
+                        ->testedInstance
+                        ->assertValue('test', 'test')
+                    ;
+                }
+            )
+            ->isInstanceOf('RREST\Exception\InvalidParameterException')
+        ;
+
+        //cast type date
+        $this->newTestedInstance('name','date',true);
+        $this
+            ->exception(
+                function() {
+                    $this
+                        ->testedInstance
+                        ->assertValue('test', 'test')
+                    ;
+                }
+            )
+            ->isInstanceOf('RREST\Exception\InvalidParameterException')
+        ;
+
+        //min
+        $this->newTestedInstance('name','integer',true);
+        $this->testedInstance->setMinimum(50);
+        $this
+            ->exception(
+                function() {
+                    $this
+                        ->testedInstance
+                        ->assertValue(30, 30)
+                    ;
+                }
+            )
+            ->isInstanceOf('RREST\Exception\InvalidParameterException')
+        ;
+        //max
+        $this->testedInstance->setMaximum(100);
+        $this
+            ->exception(
+                function() {
+                    $this
+                        ->testedInstance
+                        ->assertValue(120, 120)
+                    ;
+                }
+            )
+            ->isInstanceOf('RREST\Exception\InvalidParameterException')
+        ;
+
+        //enum
+        $this->newTestedInstance('name','integer',true);
+        $this->testedInstance->setEnum([50,100,200]);
+        $this
+            ->exception(
+                function() {
+                    $this
+                        ->testedInstance
+                        ->assertValue(120, 120)
+                    ;
+                }
+            )
+            ->isInstanceOf('RREST\Exception\InvalidParameterException')
+        ;
+
+        //pattern
+        $this->newTestedInstance('name','string',true);
+        $this->testedInstance->setValidationPattern('^\w+@[a-zA-Z_]+?.[a-zA-Z]{2,3}$');
+        $this
+            ->exception(
+                function() {
+                    $this
+                        ->testedInstance
+                        ->assertValue('not an email', 'not an email')
+                    ;
+                }
+            )
+            ->isInstanceOf('RREST\Exception\InvalidParameterException')
+        ;
+    }
 }
