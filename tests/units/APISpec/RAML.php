6906bc5b2c2d9bc1a42175d08a6e35ee2f3c2502
Fabien Furet <fabien.furet@gmail.com>
2016-02-27 18:11:59 +0100

    test: add RREST\APISpec\RAML

---
+++ tests/units/APISpec/RAML.php
@@ -0,0 +1,33 @@
+<?php
+namespace RREST\tests\units\APISpec;
+
+require_once __DIR__ . '/../boostrap.php';
+
+use atoum;
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+use RREST\Response;
+
+class RAML extends atoum
+{
+    public function testUseAuthentificationMechanism()
+    {
+        $apiDefinition = (new \Raml\Parser())->parse(__DIR__.'/../../fixture/song.raml');
+
+        $this->newTestedInstance($apiDefinition, 'GET', '/v1/songs');
+        $this
+            ->given( $this->testedInstance )
+            ->boolean($this->testedInstance->useAuthentificationMechanism())
+            ->isFalse()
+        ;
+
+        $this->newTestedInstance($apiDefinition, 'GET', '/v1/songs/85');
+        $this
+            ->given( $this->testedInstance )
+            ->boolean($this->testedInstance->useAuthentificationMechanism())
+            ->isTrue()
+        ;
+    }
+}
