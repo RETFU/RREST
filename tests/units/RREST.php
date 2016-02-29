a55dbc9f6f991d8e0f8d494e9cc7085562a1e2b7
Fabien Furet <fabien.furet@gmail.com>
2016-02-29 19:17:38 +0100

    test: add RREST\RREST

--- tests/units/RREST.php
+++ tests/units/RREST.php
@@ -4,27 +4,107 @@ namespace RREST\tests\units;
 require_once __DIR__ . '/boostrap.php';
 
 use atoum;
+use Silex\Application;
+use RREST\Provider\Silex;
+use RREST\APISpec\RAML;
 
 class RREST extends atoum
 {
+    /**
+     * @var \Raml\Parser
+     */
+    public $apiDefinition;
+
+    public function beforeTestMethod($method)
+    {
+        if(is_null($this->apiDefinition)) {
+            $this->apiDefinition = (new \Raml\Parser())->parse(__DIR__.'/../fixture/song.raml');
+        }
+    }
+
+    /**
+     * @return Silex
+     */
+    private function getSilexProvider()
+    {
+        $app = new Application();
+        $provider = new Silex($app);
+        return $provider;
+    }
+
+    /**
+     * @param  string $method
+     * @param  string $routePath
+     * @return RAML
+     */
+    public function getRAMLAPISpec($apiDefinition, $method, $routePath)
+    {
+        $apiSpec = new RAML($apiDefinition, $method, $routePath);
+        return $apiSpec;
+    }
+
     public function testGetActionMethodName()
     {
-        //FIXME: add a raml file for test purpose & fix this test
-        // $app = new \Mock\Silex\Application;
-        // $ramlCacheFile = '/home/iwd/app/git/iwd-bran/config/api/api.raml.cache';
-        // if ( file_exists($ramlCacheFile) ) {
-        //     $apiDefinition = unserialize(file_get_contents($ramlCacheFile));
-        // } else {
-        //     throw new \RuntimeException($ramlCacheFile.' is missing: execute php ./cli/cli.php build');
-        // }
-        //
-        // $apiSpec = new \Mock\RREST\APISpec\RAML($apiDefinition, 'POST', '/api/v1/authenticate');
-        // $provider = new \Mock\RREST\Provider\Silex($app);
-        //
-        // $this
-        //     ->given($this->newTestedInstance($apiSpec, $provider))
-        //     ->string($this->testedInstance->getActionMethodName('post'))
-        //     ->isEqualTo('postAction')
-        // ;
+        $apiSpec = $this->getRAMLAPISpec($this->apiDefinition, 'GET', '/v1/songs/98');
+        $provider = $this->getSilexProvider();
+
+        $this
+            ->given($this->newTestedInstance($apiSpec, $provider))
+            ->string($this->testedInstance->getActionMethodName('get'))
+            ->isEqualTo('getAction')
+        ;
+    }
+
+    public function testGetControllerNamespaceClass()
+    {
+        $apiSpec = $this->getRAMLAPISpec($this->apiDefinition, 'GET', '/v1/songs/98');
+        $provider = $this->getSilexProvider();
+
+        $this
+            ->given($this->newTestedInstance($apiSpec, $provider))
+            ->string($this->testedInstance->getControllerNamespaceClass('Songs'))
+            ->isEqualTo('Controllers\\Songs')
+        ;
+
+        $this
+            ->given($this->newTestedInstance($apiSpec, $provider, 'Path\\To\\Controllers'))
+            ->string($this->testedInstance->getControllerNamespaceClass('Songs'))
+            ->isEqualTo('Path\\To\\Controllers\\Songs')
+        ;
+    }
+
+    public function testGetProtocol()
+    {
+        $apiSpec = $this->getRAMLAPISpec($this->apiDefinition, 'GET', '/v1/songs/98');
+        $provider = $this->getSilexProvider();
+
+        $this
+            ->given($this->newTestedInstance($apiSpec, $provider))
+            ->string($this->testedInstance->getProtocol())
+            ->isEqualTo('HTTP')
+        ;
+
+        $_SERVER['HTTPS'] = true;
+        $this
+            ->given($this->newTestedInstance($apiSpec, $provider))
+            ->string($this->testedInstance->getProtocol())
+            ->isEqualTo('HTTPS')
+        ;
+
+        $_SERVER['HTTPS'] = 'on';
+        $this
+            ->given($this->newTestedInstance($apiSpec, $provider))
+            ->string($this->testedInstance->getProtocol())
+            ->isEqualTo('HTTPS')
+        ;
+
+        unset($_SERVER['HTTPS']);
+        $_SERVER['HTTP_X_FORWARDED_PROTO'] = 'https';
+        $_SERVER['HTTP_X_FORWARDED_SSL'] = 'on';
+        $this
+            ->given($this->newTestedInstance($apiSpec, $provider))
+            ->string($this->testedInstance->getProtocol())
+            ->isEqualTo('HTTPS')
+        ;
     }
 }
